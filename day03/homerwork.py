38a8456ccf60d7efc17a1ac737c48c52447296eb
dataskhirtladze <Dataskhirtladze79@gmail.com>
2024-06-05 23:54:45 +0400

    Add files via upload

---
+++ day03/homerwork.py
@@ -0,0 +1,54 @@
+#დავალება 1
+name =  "data" #აქ დავწერე ჩემი სახელი ცვლადში
+surname = "skhirtladze" #აქ დავწერე ჩემი გვარი ცვლადში
+age = 11 #აქ დავწერე ჩემი ასაკი ცვლადში
+hobby = "developer" #აქ დავწერე ჩემი ჰობი ცვლადში
+country = "georgia" #აქ დავწერე ჩემი ასაკი ცვლადში
+print(name) #აქ დავბეჭდეთ სახელი
+print(surname) #აქ დავბეჭდეთ გვარი
+print(age) #აქ დავბეჭდეთ ასაკი
+print(hobby) #აქ დავბეჭდეთ ჰობი
+print(country) #აქ დავბეჭდეთ ქვეყანა
+
+#დავალება 2 
+#მიმატებები
+print(367 + 788)
+print(849 + 123)
+print(837 + 543)
+numb1 = 653
+print(numb1 + 578)
+numb2 = 853
+numb3 = 564
+print(numb2 + numb3)
+#გამოკლებები
+print(785 - 879)
+print(875 - 874)
+print(483 - 866)
+num1 = 578
+print(num1 - 754)
+num4 = 599
+num5 = 954
+print(num4 - num5)
+#გამრავლებები
+print(347 * 438)
+print(652 * 789)
+print(847 * 394)
+name1 = 489
+print(name1 * 489)
+name2 = 498
+name3 = 785
+print(name2 * name3)
+#გაყოფა
+print(75 // 45) #ორი გაყოფის  შემთხვევაში რიცხვი იწერება მთელი
+print(67 // 48) 
+quest1 = 84
+quest2 = 74
+print(quest1 // quest2)
+quest3 = 48
+print(quest3 // 29)
+#გამრავლება
+print(35 ** 4)
+print(9 ** 9)
+numb1 = 35
+numb2 = 21
+print(numb1 ** numb2)
---
+++ day04/homerwork.py
@@ -0,0 +1,18 @@
+name = "data" #აქ დავწერე ჩემი სახელი
+surname = "skhirtladze" #აქ დავწერე ჩემი გვარი
+age = 11 #აქ დავწერე ჩემი ასაკი
+hobby = "developer" #აქ დავწერე ჩემი ჰობი
+country = "georgia" #აქ დავწერე ქვეყანა
+print(name)
+print(surname)
+print(age)
+print(hobby)
+print(country)
+
+#დავალება 2 
+name = "data" 
+surname = "skhirtladze" 
+age = "11" 
+hobby = "developer"
+country = "georgia" 
+print(name + ' ' + surname + ' ' + age + ' ' + hobby + ' ' + country)
