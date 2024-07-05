8d13c7a4c4c5ce25ecf53ad2c68dca9f04cee5ae
dataskhirtladze <Dataskhirtladze79@gmail.com>
2024-07-05 20:02:55 +0400

    Add files via upload

---
+++ homerwork/homerwork.py
@@ -0,0 +1,38 @@
+num1 =int(input("pls enter  number1:"))
+num2 =int(input("pls enter  number2:"))
+num3 =int(input("pls enter  number3:"))
+num4 =int(input("pls enter  number4:"))
+print (num1 + num2 + num3 + num4)
+print (num1 - num2 - num3 - num4)
+print (num1 * num2 * num3 * num4)
+print (num1 // num2 // num3 // num4)
+print (num1 ** num2 ** num3 ** num4)
+ 
+#2 დავალება
+my_first_sis_age = 21
+my_second_sis_age = 19
+nam1 =print(my_first_sis_age + 20)
+nam2 =print(my_second_sis_age + 20)
+print("my first sis age 20yrs later",nam1)
+print("my second sis 20yrs later",nam2)
+
+#3 დავალება
+name = "data" 
+surname = "skhirtladze"
+age = "11"
+country = "georgia"
+city = "tbilisi"
+color = "orange"
+car = "bmw m5 f90"
+food = "khinkali"
+sport = "basketball"
+print(name,surname,age,country,city,color,car,food,sport)
+
+#4 დავალება
+name = input("please enter your name:")
+print("gilocav dabadebis dges",name)
+
+#5 დავალება
+value = 4.40
+quantity = 20
+print("all price:", value * quantity)
