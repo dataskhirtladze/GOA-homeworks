19f172705ece367837c065dc78e6982c407b7878
dataskhirtladze <Dataskhirtladze79@gmail.com>
2024-07-12 17:28:21 +0400

    Add files via upload

---
+++ day09/homerwork/homerwork.py
@@ -0,0 +1,125 @@
+#1 დავალება
+nam1 = 37            
+nam2 = 15
+print(nam1 == nam2)
+
+nam1 = 64
+nam2 = 42
+print(nam1 > nam2)
+
+nam1 = 43
+nam2 = 29
+print =(nam1 < nam2)
+
+nam1 = 74
+nam2 = 46
+print =(nam1 <= nam2)
+
+nam1 = 30
+nam2 = 79
+print =(nam1 == nam2)
+
+nam1 = 89           
+nam2 = 57
+print(nam1 > nam2)
+
+nam1 = 19
+nam2 = 56
+print(nam1 < nam2)
+
+nam1 = 43
+nam2 = 29
+print =(nam1 < nam2)
+
+nam1 = 74
+nam2 = 46
+print =(nam1 <= nam2)
+
+nam1 = 68
+nam2 = 37
+print =(nam1 == nam2)
+
+nam1 = 48           
+nam2 = 29
+print(nam1 > nam2)
+
+nam1 = 36
+nam2 = 30
+print(nam1 < nam2)
+
+nam1 = 32
+nam2 = 78
+print =(nam1 <= nam2)
+
+nam1 = 50
+nam2 = 39
+print =(nam1 == nam2)
+
+nam1 = 46
+nam2 = 31
+print =(nam1 > nam2)
+
+nam1 = 36          
+nam2 = 35
+print(nam1 < nam2)
+
+nam1 = 38
+nam2 = 59
+print(nam1 <= nam2)
+
+nam1 = 49
+nam2 = 23
+print =(nam1 == nam2)
+
+nam1 = 59
+nam2 = 34
+print =(nam1 > nam2)
+
+nam1 = 74
+nam2 = 40
+print =(nam1 < nam2)
+
+nam1 = 48           
+nam2 = 29
+print(nam1 > nam2)
+
+nam1 = 37
+nam2 = 80
+print(nam1 <= nam2)
+
+nam1 = 49
+nam2 = 70
+print =(nam1 == nam2)
+
+nam1 = 47
+nam2 = 69
+print =(nam1 > nam2)
+
+nam1 = 39
+nam2 = 67
+print =(nam1 < nam2)
+
+nam1 = 48          
+nam2 = 39
+print(nam1 <= nam2)
+
+nam1 = 90
+nam2 = 62
+print(nam1 == nam2)
+
+nam1 = 10
+nam2 = 20
+print =(nam1 > nam2)
+
+nam1 = 13
+nam2 = 11
+print =(nam1 < nam2)
+
+nam1 = 29
+nam2 = 34
+print =(nam1 <= nam2)
+
+#2 დავალება
+num1 =int(input("pls enter your age1:"))
+num2 =int(input("pls enter your age2:"))
+print(num1 >= num2) 
